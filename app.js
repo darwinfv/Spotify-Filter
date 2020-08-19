57c8fe91eac8ed78d5e738ed834833576be38a49
Darwin Vaz <dvaz@purdue.edu>
2020-08-19 14:29:59 -0400

    added error handling

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.get('/', function(req, res) {
     res.send("success bits");
 });
 
+app.use(function(err, req, res, next) {
+    console.log(err);
+    console.log(req.body);
+    res.status(400).send("Wrong route");
+});
+
 //axios.get('https://neat-robin-40.serverless.social').then(function (response) {
     
 //})
