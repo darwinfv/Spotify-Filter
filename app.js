f992704314d24eefdc0e1ea2f94c47b46b335934
Darwin Vaz <dvaz@purdue.edu>
2020-08-19 15:02:41 -0400

    rename files

---
+++ app.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+    console.log("Listening on port " + port);
+});
+
+app.get('/', function(req, res) {
+    res.send("Hello world");
+});
+
+app.get('/hook', function(req, res) {
+    console.log(req.body);
+});
+
+app.use(function(err, req, res, next) {
+    console.log(err);
+    console.log(req.body);
+    res.status(400).send("Wrong route");
+});
+
