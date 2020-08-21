7caf2c7e64ad6f5a0a05be68e5f60a3437887f16
Darwin Vaz <dvaz@purdue.edu>
2020-08-20 21:07:58 -0400

    added post request for data

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const axios = require('axios');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+
+//app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.listen(port, function() {
     console.log("Listening on port " + port);
@@ -17,6 +21,12 @@ app.get('/hook', function(req, res) {
     if (body['hub.verify_token'] == 'pandabear') {
         res.send(body['hub.challenge']);
     }
+    res.send("Challenge missing");
+});
+
+app.post('/hook', function(req, res) {
+    console.log(req.body);
+    res.send("A");
 });
 
 app.use(function(err, req, res, next) {
