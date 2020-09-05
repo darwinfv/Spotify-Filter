b6eb45b6b449a2cbdf446fc1eb3282831fb0de64
Darwin Vaz <dvaz@purdue.edu>
2020-09-05 17:42:23 -0400

    set layout for playlists

--- js/playlist.js
+++ js/playlist.js
@@ -1,12 +1,18 @@
 let button = document.getElementById('playlist');
+let main = document.getElementById('main');
+let list = document.getElementById('list');
 let bg = chrome.extension.getBackgroundPage();
 let code;
 
 button.onclick = function (element) {
 
     chrome.storage.sync.get('code', function (data) {
-        code = data.code;
-        getPlaylists();
+        if (data.code) {
+            code = data.code;
+            main.hidden = true;
+            list.hidden = false;
+            getPlaylists();
+        }
     });
 
 
@@ -37,7 +43,7 @@ function getPlaylists(offset = 0, limit = 10) {
         let items = data.items;
         let covers = document.getElementsByClassName('cover');
         let imgs = document.getElementsByClassName('cover-img');
-        let title = document.getElementsByClassName('cover-span');
+        let title = document.getElementsByClassName('cover-btn');
         
         for (let i = 0; i < items.length; i++) {
             imgs[i].src = items[i].images[0].url;
