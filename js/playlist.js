b03078eae33921b09ccc1918f7afe86843fb7742
Darwin Vaz <dvaz@purdue.edu>
2020-09-04 21:28:07 -0400

    added playlist view

--- js/playlist.js
+++ js/playlist.js
@@ -1,5 +1,3 @@
-const { response } = require("express");
-
 let button = document.getElementById('playlist');
 let bg = chrome.extension.getBackgroundPage();
 let code;
@@ -34,6 +32,16 @@ function getPlaylists(offset = 0, limit = 10) {
         if (offset == 0) {
             total = data.total;
             page = 1;
+        }
+
+        let items = data.items;
+        let covers = document.getElementsByClassName('cover');
+        let imgs = document.getElementsByClassName('cover-img');
+        let title = document.getElementsByClassName('cover-span');
+        
+        for (let i = 0; i < items.length; i++) {
+            imgs[i].src = items[i].images[0].url;
+            title[i].innerHTML = items[i].name;
         }
         
     });
